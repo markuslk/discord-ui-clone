fd7ab5ad68255cc042530d37d13c72e476b859bc
Markus <markuslokk@gmail.com>
2024-07-30 14:25:58 +0300

    initial setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,30 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				brand: "#5865F2",
+				gray: {
+					50: "#ECEDEE",
+					100: "#DCDDDE",
+					200: "#B9BBBE",
+					300: "#8E9297",
+					400: "#72767D",
+					500: "#5C6067",
+					600: "#464950",
+					700: "#36393F",
+					800: "#2F3136",
+					900: "#202225",
+				},
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
