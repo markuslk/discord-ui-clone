1cf2339e11639c0a7040e15054d7c200d00eb1b6
Markus <markuslokk@gmail.com>
2024-09-18 17:23:39 +0300

    configure tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,30 +1,36 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-	content: [
-		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-	],
-	theme: {
-		extend: {
-			colors: {
-				brand: "#5865F2",
-				gray: {
-					50: "#ECEDEE",
-					100: "#DCDDDE",
-					200: "#B9BBBE",
-					300: "#8E9297",
-					400: "#72767D",
-					500: "#5C6067",
-					600: "#464950",
-					700: "#36393F",
-					800: "#2F3136",
-					900: "#202225",
-				},
-			},
-		},
-	},
-	plugins: [],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    boxShadow: {
+      sm: "0 1px 0 rgba(4,4,5,0.2),0 1.5px 0 rgba(6,6,7,0.05),0 2px 0 rgba(4,4,5,0.05)",
+      md: "0 4px 4px rgba(0,0,0,0.16)",
+      lg: "0 8px 16px rgba(0,0,0,0.24)",
+    },
+    extend: {
+      colors: {
+        brand: "#5865F2",
+        gray: {
+          50: "#ECEDEE",
+          100: "#DCDDDE",
+          200: "#B9BBBE",
+          300: "#8E9297",
+          400: "#72767D",
+          500: "#5C6067",
+          550: "#4F545C",
+          600: "#464950",
+          700: "#36393F",
+          800: "#2F3136",
+          900: "#202225",
+        },
+      },
+    },
+  },
+  plugins: [],
 };
 export default config;
