ac8c95e90e9c81e80daf88d6ab4fb8f065bc13db
Markus <markuslokk@gmail.com>
2024-09-19 17:25:38 +0300

    refactor layouts for dynamic data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,12 @@
 "use client";
+import { Home } from "lucide-react";
 import { Inter } from "next/font/google";
-import "./globals.css";
-import { Activity, Airplay, AppWindow, Home } from "lucide-react";
-import NavLink from "./components/NavLink";
 import { useParams } from "next/navigation";
+import NavLink from "./components/NavLink";
+import "./globals.css";
+import { data } from "./servers/[serverId]/layout";
 
 const inter = Inter({ subsets: ["latin"] });
-
-let servers = [
-  { id: "1", icon: Activity },
-  { id: "2", icon: Airplay },
-  { id: "3", icon: AppWindow },
-];
 
 export default function RootLayout({
   children,
@@ -24,16 +19,16 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <main className="flex h-screen text-gray-100">
-          <div className="space-y-2 bg-gray-900 p-3">
+          <div className="hidden space-y-2 overflow-y-auto bg-gray-900 p-3 md:block">
             <NavLink href="/">
               <Home />
             </NavLink>
             <hr className="mx-2 rounded border-t-2 border-t-white/[.06]" />
-            {servers.map((server) => (
+            {data.map((server) => (
               <NavLink
                 key={server.id}
                 active={+serverId === +server.id}
-                href={`/servers/${server.id}/channels/1`}
+                href={`/servers/${server.id}/channels/${server.categories[0].channels[0].id}`}
               >
                 <server.icon />
               </NavLink>
--- src/app/servers/[serverId]/layout.tsx
+++ src/app/servers/[serverId]/layout.tsx
@@ -1,5 +1,8 @@
 "use client";
 import {
+  Activity,
+  Airplay,
+  AppWindow,
   Badge,
   BookCheck,
   Check,
@@ -10,19 +13,36 @@ import {
   UserPlus,
 } from "lucide-react";
 import Link from "next/link";
-import { ForwardRefExoticComponent, ReactNode, RefAttributes } from "react";
 import { useParams } from "next/navigation";
+import {
+  ForwardRefExoticComponent,
+  ReactNode,
+  RefAttributes,
+  useState,
+} from "react";
+import { format } from "date-fns";
+import { faker } from "@faker-js/faker";
+import { Message } from "./channels/[channelId]/page";
 
-const ServerPageLayout = ({
-  params,
-  children,
-}: {
-  params: { serverId: string };
-  children: ReactNode;
-}) => {
+faker.seed(123);
+
+const ServerPageLayout = ({ children }: { children: ReactNode }) => {
+  const [closedCategories, setClosedCategories] = useState<Number[]>([]);
+  const { serverId } = useParams();
+
+  const server = data.find((server) => +server.id === +serverId);
+
+  function toggleCategory(categoryId: number) {
+    setClosedCategories((closedCategories) =>
+      closedCategories.includes(categoryId)
+        ? closedCategories.filter((id) => id !== categoryId)
+        : [...closedCategories, categoryId],
+    );
+  }
+
   return (
     <>
-      <div className="flex w-60 flex-col bg-gray-800">
+      <div className="hidden w-60 flex-col bg-gray-800 md:flex">
         <button className="flex h-12 items-center px-4 text-[15px] font-bold shadow-md transition hover:bg-gray-550/[0.16]">
           <div className="relative mr-1 flex h-4 w-4 items-center justify-center">
             <Badge className="absolute size-5 fill-gray-550 text-gray-550" />
@@ -32,51 +52,57 @@ const ServerPageLayout = ({
           <ChevronDown className="ml-auto size-[18px] opacity-80" />
         </button>
         <div className="flex-1 space-y-[21px] overflow-y-auto py-3 font-medium text-gray-300">
-          {data["1"].categories.map((category) => (
+          {server?.categories.map((category) => (
             <div key={category.id}>
               {category.label && (
-                <button className="flex w-full items-center px-0.5 text-xs uppercase tracking-wide hover:text-gray-100">
-                  <ChevronDown className="mr-0.5 size-3" />
+                <button
+                  onClick={() => toggleCategory(category.id)}
+                  className="flex w-full items-center px-0.5 text-xs uppercase tracking-wide hover:text-gray-100"
+                >
+                  <ChevronDown
+                    className={`${closedCategories.includes(category.id) ? "-rotate-90" : ""} mr-0.5 size-3`}
+                  />
                   {category.label}
                 </button>
               )}
               <div className="mt-[5px] space-y-0.5">
-                {category.channels.map((channel) => (
-                  <ChannelLink
-                    key={channel.id}
-                    channel={channel}
-                    server={params.serverId}
-                  />
-                ))}
+                {category.channels
+                  .filter((channel: Channel) => {
+                    let categoryIsOpen = !closedCategories.includes(
+                      category.id,
+                    );
+
+                    return categoryIsOpen || channel.unread;
+                  })
+                  .map((channel) => (
+                    <ChannelLink key={channel.id} channel={channel} />
+                  ))}
               </div>
             </div>
           ))}
         </div>
       </div>
 
-      <div>{children}</div>
+      <>{children}</>
     </>
   );
 };
 export default ServerPageLayout;
 
-type Channel = {
+export type Channel = {
   id: number;
   label: string;
   icon?: ForwardRefExoticComponent<
     Omit<LucideProps, "ref"> & RefAttributes<SVGSVGElement>
   >;
+  description?: string;
   unread?: boolean;
+  messages: Message[];
 };
 
-const ChannelLink = ({
-  channel,
-  server,
-}: {
-  channel: Channel;
-  server: string;
-}) => {
+const ChannelLink = ({ channel }: { channel: Channel }) => {
   const { id, label, icon: Icon } = channel;
+  const { serverId } = useParams();
   const { channelId } = useParams();
   let active = +channel.id === +channelId;
   let state: "active" | "inactiveUnread" | "inactiveRead" = active
@@ -94,7 +120,7 @@ const ChannelLink = ({
   return (
     <Link
       key={id}
-      href={`/servers/${server}/channels/${channel.id}`}
+      href={`/servers/${serverId}/channels/${channel.id}`}
       className={`${classes[state]} group relative mx-2 flex items-center rounded px-2 py-1 font-normal`}
     >
       {state === "inactiveUnread" && (
@@ -111,56 +137,288 @@ const ChannelLink = ({
   );
 };
 
-let data = {
-  "1": {
+export const data = [
+  {
+    id: 1,
     label: "Tailwind CSS",
+    icon: Activity,
     categories: [
       {
         id: 1,
         label: "",
         channels: [
-          { id: 1, label: "welcome", icon: BookCheck },
-          { id: 2, label: "announcements", icon: Megaphone },
+          {
+            id: 1,
+            label: "welcome",
+            icon: BookCheck,
+            description:
+              "Introduction to the Tailwind CSS framework and community.",
+            messages: getMessages(),
+          },
+          {
+            id: 2,
+            label: "announcements",
+            icon: Megaphone,
+            messages: getMessages(),
+          },
         ],
       },
       {
         id: 2,
         label: "Tailwind CSS",
         channels: [
-          { id: 3, label: "general", unread: true },
-          { id: 4, label: "plugins", unread: true },
-          { id: 5, label: "help" },
-          { id: 6, label: "internals" },
+          {
+            id: 3,
+            label: "general",
+            unread: true,
+            description:
+              "General discussion of Tailwind CSS (please move off-topic discussion in the off-topic channels).",
+            messages: getMessages(),
+          },
+          {
+            id: 4,
+            label: "plugins",
+            unread: true,
+            description: "Tailwind CSS plugins.",
+            messages: getMessages(),
+          },
+          {
+            id: 5,
+            label: "help",
+            description:
+              "Help with Tailwind CSS and build process integration.",
+            messages: getMessages(),
+          },
+          {
+            id: 6,
+            label: "internals",
+            description: "Development of the Tailwind CSS framework itself.",
+            messages: getMessages(),
+          },
         ],
       },
       {
         id: 3,
         label: "Tailwind Labs",
         channels: [
-          { id: 7, label: "tailwind-ui", unread: true },
-          { id: 8, label: "headless-ui" },
-          { id: 9, label: "refactoring-ui" },
-          { id: 10, label: "heroicons", unread: true },
+          {
+            id: 7,
+            label: "tailwind-ui",
+            unread: true,
+            description: "General discussion of Tailwind UI.",
+            messages: getMessages(),
+          },
+          {
+            id: 8,
+            label: "headless-ui",
+            description: "General discussion of Headless UI.",
+            messages: getMessages(),
+          },
+          {
+            id: 9,
+            label: "refactoring-ui",
+            description: "General discussion of Refactoring UI.",
+            messages: getMessages(),
+          },
+          {
+            id: 10,
+            label: "heroicons",
+            unread: true,
+            description: "General discussion of Heroicons.",
+            messages: getMessages(),
+          },
         ],
       },
       {
         id: 4,
         label: "Off topic",
         channels: [
-          { id: 11, label: "design" },
-          { id: 12, label: "development", unread: true },
-          { id: 13, label: "random", unread: true },
+          {
+            id: 11,
+            label: "design",
+            description: "General discussion of web design.",
+            messages: getMessages(),
+          },
+          {
+            id: 12,
+            label: "development",
+            unread: true,
+            description: "General discussion of web development.",
+            messages: getMessages(),
+          },
+          {
+            id: 13,
+            label: "random",
+            unread: true,
+            description: "General discussion of everything else!",
+            messages: getMessages(),
+          },
         ],
       },
       {
         id: 5,
         label: "Community",
         channels: [
-          { id: 14, label: "jobs" },
-          { id: 15, label: "showcase" },
-          { id: 16, label: "bots", unread: true },
+          {
+            id: 14,
+            label: "jobs",
+            description:
+              "Job board. Please put [HIRING] or [FOR HIRE] at the beginning of your post.",
+            messages: getMessages(),
+          },
+          {
+            id: 15,
+            label: "showcase",
+            description: "Share your projects built with Tailwind CSS!",
+            messages: getMessages(),
+            unread: true,
+          },
+          {
+            id: 16,
+            label: "bots",
+            description: "Bot spam containment.",
+            messages: getMessages(),
+          },
         ],
       },
     ],
   },
-};
+  {
+    id: 2,
+    label: "Next.js",
+    icon: Airplay,
+    categories: [
+      {
+        id: 6,
+        label: "",
+        channels: [
+          {
+            id: 17,
+            label: "welcome",
+            icon: BookCheck,
+            messages: getMessages(),
+          },
+          {
+            id: 18,
+            label: "announcements",
+            icon: Megaphone,
+            description:
+              "Announcements related to this Discord server and Next.js",
+            messages: getMessages(),
+          },
+          {
+            id: 19,
+            label: "introductions",
+            unread: true,
+            description:
+              "Welcome to the server! Feel free to introduce yourself",
+            messages: getMessages(),
+          },
+        ],
+      },
+      {
+        id: 7,
+        label: "Need-Help",
+        channels: [
+          {
+            id: 20,
+            label: "community-help",
+            description:
+              "Members of the community can help each other here, but we recommend checking GitHub discussions first: ",
+            messages: getMessages(),
+          },
+        ],
+      },
+      {
+        id: 8,
+        label: "Community",
+        channels: [
+          {
+            id: 21,
+            label: "general",
+            description: "Discussions about Next.js in general",
+            messages: getMessages(),
+          },
+          {
+            id: 22,
+            label: "off-topic",
+            unread: true,
+            description:
+              "Discussions about topics not related to Next.js or other channels",
+            messages: getMessages(),
+          },
+          {
+            id: 23,
+            label: "showcase",
+            unread: true,
+            messages: getMessages(),
+          },
+          {
+            id: 24,
+            label: "jobs-board",
+            description:
+              "Is your company looking for Next.js developers? Discuss here!",
+            messages: getMessages(),
+          },
+          {
+            id: 25,
+            label: "hire-me",
+            unread: true,
+            description: "Are you a developer looking to work with Next.js?",
+            messages: getMessages(),
+          },
+          {
+            id: 26,
+            label: "makers",
+            description:
+              "Share as you build in public. Welcoming all makers and indie hackers.",
+            messages: getMessages(),
+          },
+          {
+            id: 27,
+            label: "moderation-feedback",
+            description:
+              "Discussion about this Discord server and moderation topics",
+            messages: getMessages(),
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 3,
+    label: "Mirage JS",
+    icon: AppWindow,
+    categories: [
+      {
+        id: 9,
+        label: "Text Channels",
+        channels: [
+          { id: 28, label: "general", messages: getMessages() },
+          { id: 29, label: "graphql", unread: true, messages: getMessages() },
+          {
+            id: 30,
+            label: "typescript",
+            unread: true,
+            messages: getMessages(),
+          },
+        ],
+      },
+    ],
+  },
+];
+
+function getMessages() {
+  return [...Array(faker.number.int({ min: 7, max: 25 }))]
+    .map(() => {
+      let user = faker.internet.userName();
+
+      return [...Array(faker.number.int({ min: 1, max: 4 }))].map(() => ({
+        id: faker.number.int(),
+        user,
+        date: format(new Date(faker.date.past()), "MM/dd/yyyy"),
+        text: faker.lorem.sentences(3),
+      }));
+    })
+    .flat();
+}
